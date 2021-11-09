8ed6f4c919f95ec6f5ec72735a978d53b343344d
Henrik Gudmundsson <henrik.gudmundsson@gmail.com>
2021-11-09 12:46:50 +0100

    feat: a sixth feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          a fifth feature
+          a sixth feature
         </a>
       </header>
     </div>
